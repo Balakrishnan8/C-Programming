e7f74b674fc19b6df63fefa89a0d0df21aeb48d7
Balakrishnan8 <balakrishnanbb1078@gmail.com>
2022-11-14 18:55:00 +0530

    Control Statements

--- Control-Statement/04.switch.c
+++ Control-Statement/04.switch.c
@@ -4,6 +4,135 @@
 
 int main()
 {
+	/*
+		alternative to if-else-if ladder statement.
+		allows us to execute multiple operations for the different possible values of single variable called switch variable.
+		
+		syntax:
+		
+				switch(expression)
+				{
+					case value1:
+						//block of code
+						break;
+						
+					case value2:
+						//block of code
+						break;
+						
+					   ....
+						
+					default:
+						//executes, If all cases are not matched
+				}
+				
+		
+			-> The switch expression and case value must be Integer or Character type.
+			-> break in the case statement is optional, if the break is not used inside each case, then all the case will be executed.
+			-> Default case will execute if all the cases is not matched.
+			-> No two cases have same values.
+			-> if the matched case contains break statement,then all the case after that will be skipped and control goes to out of the switch statement.
+			-> warning: switch condition has boolean value [-switch-bool]
+			The warning is simply telling you that you're using switch statement with boolean value. For true boolean values, you should just use if - else statement, not switch . Note, you will only get this warning if you use true boolean type.
+	
+	*/
+
+
+	// examples.
+
+	int number=0;
+	
+	printf("Enter the number: ");
+	scanf("%d",&number);
+	
+	switch(number)
+	{
+		case 0:
+			printf("It is Zero\n");
+			break;
+			
+		case 1:
+			printf("It is a one\n");
+			break;
+			
+		case 2:
+			printf("It is a two\n");
+			break;
+			
+		default:
+			printf("greater than two\n");	
+	
+	}
+
+
+	// Finding grade for given mark
+
+	int mark;
+	
+	printf("Enter Your Mark: ");
+	scanf("%d",&mark);
+	
+	if(mark<=100)
+	{	
+		switch(mark/10)
+		{
+			case 10:
+				printf("Your grade is \'O\'\n");
+				break;
+				
+			case 9:
+				printf("Your grade is \'A+\'\n");
+				break;
+				
+			case 8:
+				printf("Your grade is \'A\'\n");
+				break;
+				
+			case 7:
+				printf("Your grade is \'B+\'\n");
+				break;
+				
+			case 6:
+				printf("Your grade is \'B\'\n");
+				break;
+				
+			case 5:
+				printf("Your grade is \'C\'\n");
+				break;
+			
+			case 4:
+				printf("Your Grade is \'D\'\n");
+				break;
+				
+			case 3:
+			case 2:
+			case 1:
+				printf("You are Fail.\n");
+				break;
+				
+			default:
+				printf("Please Enter Valid Mark.\n");
+		
+		}
+	}
+	else
+		printf("Your mark is more than 100. Kindly enter Correct mark\n");
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Control-Statement/05.Cloops.c
@@ -0,0 +1,57 @@
+#include"stdio.h"
+
+// loops in C.
+
+int main()
+{
+	/*
+		-> Looping can be defined as the same process multiple times until the specific condition met.
+		-> Loops provide code reusability. Instead of using same code again and again, we can use loops.
+		-> Loops used for traversing an array.
+		-> Types of Loops:
+		
+		1) do while
+				
+			-> do-while loops continues until a given conditions met.
+			-> it is post tested loop.(condition will be checked after one execution)
+			-> mostly used for menu driven programs.
+			
+			syntax:
+				do
+				{
+					// block of codes;
+				}while(condition);
+				
+			
+		2) while
+			
+			-> while loop is used when number of iteration is unknown. while loop executes until the specified condition met.
+			-> it is pre-tested loop.(condition will be first, if only the condition is true, then loop will be executed.)
+			-> mostly used when the number of iteration is unknown in advance.
+			
+			syntax:
+				while(expression)
+				{
+					// block of codes;
+				}
+			
+			
+		3) for
+		
+			-> for loop is used when some lines of codes are executed repeatedly until the specified condition is met.
+			-> it is pre-tested loop.
+			-> mostly used when number of iteration is known in advance.
+			
+			
+			syntax:
+				for(initialization;condition;incre/decre)
+				{
+					// block of codes;
+				}	
+	*/
+	
+	printf("Loops in C.\n");
+	
+	return 0;
+}
+
